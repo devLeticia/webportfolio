c55b4b9a7e28b73b90a8130e95190c0e1b5ae402
Leticia Gonçalves <leticiagoncalves.tech@gmail.com>
2023-05-22 23:37:31 -0300

    adding sections, studies details, imagens and logos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,151 @@
+'use client'
+
 import Image from 'next/image'
+import { Palette, GraduationCap } from 'phosphor-react'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
+    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+      <div className='w-full max-w-5xl flex justify-between'>
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
+          src='/logo.svg'
+          alt='Leticia Goncalves Logo'
+          className='dark:invert'
+          width={30}
+          height={24}
           priority
         />
+        <ul className='flex gap-6'>
+          <li>Projects</li>
+          <li>Stacks</li>
+          <li>Work</li>
+          <li>Studies</li>
+          <li>Contact</li>
+        </ul>
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className='z-10 flex flex-col w-full max-w-5xl items-center text-center'>
+        <p className='text-violet-400 text-lg mt-32 mb-7 tracking-wider'>
+          LETÍCIA GONÇALVES
+        </p>
+        <h1 className='text-white mb-14'>
+          Building digital products,<br></br> brands, and experience.
+        </h1>
+        <p className='text-xl'>
+          I am a software developer based in Brazil,<br></br> I specialize in
+          front-end development and UI/UX Design.
+        </p>
+        <button className='my-14'>Get in touch</button>
+        {/* <ul className='my-32 flex gap-6 text-7xl font-medium text-zinc-800'>
+          <li>Projects/</li>
+          <li>Stacks/</li>
+          <li>Work/</li>
+          <li>Studies/</li>
+          <li>Contact/</li>
+          <li>Projects/</li>
+          <li>Stacks/</li>
+          <li>Work/</li>
+          <li>Studies/</li>
+          <li>Contact/</li>
+        </ul> */}
       </div>
+      <section className='mt-14'>
+        <h3>Stacks</h3>
+      </section>
+      <section className='mt-14'>
+        <h3>Work Experience</h3>
+      </section>
+      <section className='mt-14'>
+        <h3 className='text-center'>Studies</h3>
+        {/* courses */}
+        <div className='grid grid-cols-2 mt-16 gap-3 max-w-5xl'>
+          <div className='flex flex-col gap-10 justify-between p-14'>
+            {courses.map((course) => {
+              return (
+                <div key={course.name} className='flex justify-start gap-6'>
+                  <Palette size={72} color={'#907ceb'} />
+                  <div className='flex flex-col'>
+                    <h5>{course.name}</h5>
+                    <h6 className='mt-3 text-purple-900 uppercase font-medium tracking-wider'>
+                      {course.institution}
+                    </h6>
+                    <p className='mt-6'>{course.description}</p>
+                    <span className='mt-3 text-zinc-700'>{course.year}</span>
+                  </div>
+                </div>
+              )
+            })}
+          </div>
+          <div className='flex flex-col gap-10 bg-neutral-950 pl-14 pb-14 rounded-lg'>
+            <div className='flex w-full justify-between'>
+              <GraduationCap size={42} color={'#907ceb'} className='mt-14' />
+              <Image
+                src='/globe.svg'
+                alt='Earth Globe'
+                className='dark:invert -mb-36'
+                width={316}
+                height={316}
+                priority
+              />
+            </div>
+            {graduations.map((grad) => {
+              return (
+                <div key={grad.name} className='flex flex-col w-2/3'>
+                  <h5 className='mb-3'>{grad.name}</h5>
+                  {grad.universities.map((uni) => {
+                    return (
+                      <h6
+                        key={uni}
+                        className='text-purple-900 uppercase font-medium'
+                      >
+                        {uni}
+                      </h6>
+                    )
+                  })}
+                  <span className='mt-3 text-zinc-700'>
+                    {grad.conclusionYear}
+                  </span>
+                </div>
+              )
+            })}
+          </div>
+        </div>
+      </section>
     </main>
   )
 }
+
+const courses = [
+  {
+    icon: 'code-block',
+    name: 'Ignite Bootcamp',
+    institution: 'ROCKETSEAT',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore',
+    year: 'ongoing',
+  },
+  {
+    icon: 'pallete',
+    name: 'Product Design, User Experience and Interfaces',
+    institution: 'INTERACTION DESIGN FOUNDATION',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore',
+    year: 'ongoing',
+  },
+]
+
+const graduations = [
+  {
+    name: 'System Analisys and Development',
+    universities: ['ESTACIO DE SÁ UNVIVERSTISTY'],
+    conclusionYear: 2022,
+  },
+  {
+    name: 'International Relations',
+    universities: [
+      'PUC-RIO',
+      'UNIVERSITY OF SOUTH CAROLINA',
+      'FREE UNIVERSITY OF BERLIN',
+    ],
+    conclusionYear: 2016,
+  },
+]
